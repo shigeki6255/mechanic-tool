c296f6520a18e9954ae89d878aa5df496324713d
shigeki6255 <kcjh.6255@gmail.com>
2026-09-25 23:59:23 +0900

    Update materials.js

--- materials.js
+++ materials.js
@@ -1,33 +1,48 @@
 const materials = {
 
-  engineParts: {
-    name: "エンジン部品",
+  skit: {
+    name: "修理キット",
     price: 50000
+  },
+
+  rub: {
+    name: "ゴム",
+    price: 10000
+  },
+
+  kuzu: {
+    name: "金属くず",
+    price: 12000
+  },
+
+  bolt: {
+    name: "ネジ部品",
+    price: 15000
+  },
+
+  metal: {
+    name: "鋼材",
+    price: 35000
+  },
+
+  fire: {
+    name: "点火プラグ",
+    price: 25000
+  },
+
+  battery: {
+    name: "カーバッテリー",
+    price: 90000
   },
 
   oil: {
     name: "エンジンオイル",
-    price: 10000
+    price: 50000
   },
 
-  filter: {
-    name: "オイルフィルター",
-    price: 5000
-  },
-
-  bodyParts: {
-    name: "ボディ部品",
-    price: 40000
-  },
-
-  battery: {
-    name: "バッテリー",
-    price: 25000
-  },
-
-  tire: {
-    name: "タイヤ",
-    price: 35000
+  dnc: {
+    name: "電池",
+    price: 20000
   }
 
 };
